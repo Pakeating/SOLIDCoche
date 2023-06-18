9735fd5cae87426a8f159813a8938b0e3f051c34
Paco <paco.refractando@gmail.com>
2023-06-18 22:38:31 +0200

    a falta de aplicar el tercer principio

--- src/main/java/com/prog/cochesolid/CocheCombustible.java
+++ src/main/java/com/prog/cochesolid/CocheCombustible.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.prog.cochesolid;
 
 /**
@@ -9,5 +6,9 @@ package com.prog.cochesolid;
  * @author PAKEATING
  */
 public class CocheCombustible extends CocheMovimiento {
-    
+   
+    @Override
+    public void arrancar(){
+        
+    }
 }
